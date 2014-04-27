e5130a7bb0abf872e4380ea5716d0f041c50f938
Marcelo J. Both <marcelo.both@gmail.com>
2014-04-27 15:08:46 -0300

    Change name extjs_scaffold to extjs-scaffold

--- extjs-scaffold.gemspec
+++ extjs-scaffold.gemspec
@@ -24,5 +24,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency "cucumber-rails"
   s.add_development_dependency "aruba", "< 0.4.7"
   s.add_runtime_dependency "rails", "~> 4.0.0"
-  s.add_runtime_dependency "extjs4-rails"
+  #s.add_runtime_dependency "extjs4-rails"
 end
--- lib/generators/extjs-scaffold/scaffold_controller/scaffold_controller_generator.rb
+++ lib/generators/extjs-scaffold/scaffold_controller/scaffold_controller_generator.rb
@@ -1,19 +1,19 @@
 require 'generators/extjs_scaffold'
 require 'rails/generators/resource_helpers'
 
-module ExtjsScaffold
+module Extjs4Scaffold
   module Generators
     class ScaffoldControllerGenerator < Base
-      
+
       include Rails::Generators::ResourceHelpers
-      
+
       argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
-      
-      class_option :file_name, :desc => "Name of file used to hold Ext.application", 
-              :aliases => '-n', :default =>ExtjsScaffold::Generators::Base.rails_app_name
-      class_option :app_name, :desc => "Name of app used in Ext.application", 
-              :aliases => '-a', :default => ExtjsScaffold::Generators::Base.rails_app_name
-      
+
+      class_option :file_name, :desc => "Name of file used to hold Ext.application",
+              :aliases => '-n', :default =>Extjs4Scaffold::Generators::Base.rails_app_name
+      class_option :app_name, :desc => "Name of app used in Ext.application",
+              :aliases => '-a', :default => Extjs4Scaffold::Generators::Base.rails_app_name
+
       class_option :orm, :desc => "ORM used to generate the controller"
       class_option :template_engine, :desc => "Template engine to generate view files"
       class_option :views, :type => :boolean, :default => true
@@ -22,7 +22,7 @@ module ExtjsScaffold
       class_option :reference_fields, :type => :hash, :desc => "Collection of fields to use for one table lookup: --reference_fields parent_table:field_name
                                            # Default: parent_table:name"
       class_option :test_framework, :desc => "Test framework to be invoked"
-      
+
       # add class method 'search' to model
       def create_model_methods
         @pagination = options.pagination
@@ -41,7 +41,7 @@ module ExtjsScaffold
           remove_file "app/models/#{controller_file_name}_tmp.rb"
         end
       end
-      
+
       check_class_collision :suffix => "Controller"
 
       def create_controller_files
@@ -64,7 +64,7 @@ module ExtjsScaffold
           filename = [name, :js].compact.join(".")
           case name
           when 'Controller'
-            template "js/#{filename}", File.join("app/assets/javascripts/controller", "#{plural_table_name.capitalize}.js")  
+            template "js/#{filename}", File.join("app/assets/javascripts/controller", "#{plural_table_name.capitalize}.js")
           when 'Model'
             template "js/#{filename}", File.join("app/assets/javascripts/model", "#{singular_table_name.capitalize}.js")
           when 'Store'
@@ -74,7 +74,7 @@ module ExtjsScaffold
           end
         end
       end
-      
+
       # create stores for any reference lookup combos
       def create_reference_stores
         attributes.select {|attr| attr.reference? }.each do |attribute|
@@ -83,7 +83,7 @@ module ExtjsScaffold
           template "js/#{filename}", File.join("app/assets/javascripts/store", "#{singular_table_name.capitalize}#{attribute.name.capitalize.pluralize}.js")
         end
       end
-      
+
       def update_application_js
         app_init = "\n"
         app_init << "      // #{plural_table_name.capitalize}: Initialize controller and create list grid \n"
@@ -92,10 +92,10 @@ module ExtjsScaffold
         app_init << "        controller.init();\n"
         app_init << "        Ext.create('#{app_name}.view.#{singular_table_name}.Grid',{renderTo: Ext.getBody() });\n"
         app_init << "      }\n"
-        
+
         insert_into_file "app/assets/javascripts/#{app_file_name}", app_init, :after => "launch: function() {"
       end
-      
+
       def add_resource_route
         return unless options[:routes]
         route_config =  class_path.collect{|namespace| "namespace :#{namespace} do " }.join(" ")
@@ -108,15 +108,15 @@ module ExtjsScaffold
         route_config << " end" * class_path.size
         route route_config
       end
-      
+
       # copy view templates or hook to :template_engine
-      
+
       def copy_view_files
         return unless options[:views]
         empty_directory File.join("app/views", controller_file_path)
         # accept haml or default to erb
         template = options[:template_engine] == 'haml' ? options[:template_engine] : 'erb'
-        
+
         available_views.each do |view|
           filename = filename_with_extensions(view, :html, template)
           template "views/#{template}/#{filename}", File.join("app/views", controller_file_path, filename)
@@ -131,17 +131,17 @@ module ExtjsScaffold
           template "tests/controller_test.rb", File.join("test/functional", "#{controller_file_name}_controller_test.rb")
         end
       end
-      
+
       protected
-      
+
       def app_file_name
         [options.file_name, :js].compact.join(".")
       end
-      
+
       def app_name
         options.app_name
       end
-      
+
       def available_views
         %w(index)
       end
@@ -149,19 +149,19 @@ module ExtjsScaffold
       def filename_with_extensions(name, prefix, suffix)
         [name, prefix, suffix].compact.join(".")
       end
-      
+
       def available_js
         %w(Controller Model Store Grid EditForm EditWindow UpdateForm UpdateWindow)
       end
-      
+
       def reference_store
         return 'ReferenceStore'
       end
-      
+
       def reference_model
         return 'ReferenceModel'
       end
-      
+
       def reference_field(attribute)
         if options.reference_fields && options.reference_fields[attribute.name]
           options.reference_fields[attribute.name]
@@ -169,7 +169,7 @@ module ExtjsScaffold
           'name'
         end
       end
-      
+
       def create_controller_model_list
         list = []
         attributes.select {|attr| attr.reference? }.each do |attribute|
@@ -177,7 +177,7 @@ module ExtjsScaffold
         end
         return list.join(',')
       end
-      
+
       def create_controller_store_list
         list = []
         attributes.select {|attr| attr.reference? }.each do |attribute|
@@ -185,7 +185,7 @@ module ExtjsScaffold
         end
         return list.join(',')
       end
-      
+
       def create_ext_record(attribute)
         if attribute.reference?
           return "name: '#{attribute.name}_#{reference_field(attribute)}'"
@@ -200,11 +200,11 @@ module ExtjsScaffold
         	end
         end
       end
-      
+
       def create_ext_column(attribute)
         if attribute.reference?
           return "dataIndex: '#{attribute.name}_#{reference_field(attribute)}', header: '#{attribute.name.titleize}', width: 120, sortable: true"
-        else  
+        else
           case attribute.type.to_s
           when 'boolean'
             return "dataIndex: '#{attribute.name}', header: '#{attribute.name.titleize}', width: 80, renderer: #{app_name}.util.Format.booleanRenderer(), sortable: true"
@@ -215,11 +215,11 @@ module ExtjsScaffold
           end
         end
       end
-      
+
       def create_ext_formfield(attribute)
         if attribute.reference?
-          return "{ id: '#{attribute.name}_#{reference_field(attribute)}', 
-            fieldLabel: '#{attribute.name.titleize}', 
+          return "{ id: '#{attribute.name}_#{reference_field(attribute)}',
+            fieldLabel: '#{attribute.name.titleize}',
             name: '[#{singular_table_name}]#{attribute.name}_id',
             store: #{app_name}.store.#{singular_table_name.capitalize}#{attribute.name.capitalize.pluralize},
             displayField:'#{reference_field(attribute)}',
@@ -242,7 +242,7 @@ module ExtjsScaffold
           end
         end
       end
-      
+
       def create_ext_updateformfield(attribute)
         # build field container with disable checkbox
         field = "{ xtype: 'fieldcontainer', fieldLabel: '#{field_label(attribute)}', layout: 'hbox', width: #{updatefield_width(attribute)}, combineErrors: true,
@@ -251,8 +251,8 @@ module ExtjsScaffold
 							,{ xtype: 'checkboxfield', width: 20, hideLabel: true, style: 'margin-left: 5px'}
 							"
         if attribute.reference?
-          field += ",{ id: '#{attribute.name}_#{reference_field(attribute)}', 
-            hideLabel: true, 
+          field += ",{ id: '#{attribute.name}_#{reference_field(attribute)}',
+            hideLabel: true,
             name: '[#{singular_table_name}]#{attribute.name}_id',
             store: #{app_name}.store.#{singular_table_name.capitalize}#{attribute.name.capitalize.pluralize},
             displayField:'#{reference_field(attribute)}',
@@ -276,23 +276,23 @@ module ExtjsScaffold
             field += ",{id: '#{attribute.name}', name: '[#{singular_table_name}]#{attribute.name}', hideLabel: true, width: 500, flex: 1, disabled: true, xtype: 'textfield'}"
           end
         end
-        
+
         field += "
 			      ]
 	      }
-  
-        "    
+
+        "
         return field
       end
-      
+
       def field_label(attribute)
-        return attribute.type.to_s == 'boolean' ? "#{attribute.name.titleize}?" : attribute.name.titleize    
+        return attribute.type.to_s == 'boolean' ? "#{attribute.name.titleize}?" : attribute.name.titleize
       end
-      
-      def updatefield_width(attribute) 
+
+      def updatefield_width(attribute)
         return %w(boolean date integer).include?(attribute.type.to_s) ? 275 : 575
       end
-      
+
       private
 
         def resource_attributes
@@ -311,7 +311,7 @@ module ExtjsScaffold
         def accessible_attributes
           attributes.reject(&:reference?)
         end
-        
+
         def reference_attributes
           attributes.select(&:reference?)
         end
